4ae777fee71da8686502525c64a90a4be670d88f
Sylvain Bugat <cidian@laposte.net>
2015-09-05 23:18:20 +0200

    Fix compilation errors

--- src/main/java/com/github/sbugat/samplerest/config/ApplicationConfig.java
+++ src/main/java/com/github/sbugat/samplerest/config/ApplicationConfig.java
@@ -1,16 +1,24 @@
+package com.github.sbugat.samplerest.config;
 
+import javax.inject.Inject;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan("com.github.sbugat.samplerest")
 @PropertySource("classpath:application-configuration.properties")
 public class ApplicationConfig {
-	
+
 	@Inject
 	private Environment environment;
-	
+
 	@Bean
-	public static PropertyPlaceholderConfigurer ppc() {
+	public static PropertySourcesPlaceholderConfigurer ppc() {
 		// instantiate, configure and return ppc ...
 		return new PropertySourcesPlaceholderConfigurer();
 	}
