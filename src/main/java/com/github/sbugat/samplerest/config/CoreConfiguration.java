020082300a5d34e10f2381ae67b81a6510a9fcf8
Sylvain Bugat <cidian@laposte.net>
2015-09-06 23:57:09 +0200

    WIP application configuration rework

--- src/main/java/com/github/sbugat/samplerest/config/CoreConfiguration.java
+++ src/main/java/com/github/sbugat/samplerest/config/CoreConfiguration.java
@@ -1,9 +1,13 @@
 package com.github.sbugat.samplerest.config;
 
+import javax.inject.Inject;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.github.sbugat.samplerest.resource.SampleExceptionMapper;
@@ -13,7 +17,19 @@ import io.swagger.jaxrs.listing.SwaggerSerializers;
 
 @Configuration
 @ComponentScan("com.github.sbugat.samplerest")
+@PropertySource("classpath:environment/${" + CoreConfiguration.ENVIRONMENT_TYPE + ':' + CoreConfiguration.DEFAULT_ENVIRONMENT_TYPE + "}/application-configuration.properties")
 public class CoreConfiguration {
+
+	static final String ENVIRONMENT_TYPE = "environment.type";
+	static final String DEFAULT_ENVIRONMENT_TYPE = "development";
+
+	@Inject
+	private Environment environment;
+
+	@Bean
+	public String environmentType() {
+		return environment.getProperty(ENVIRONMENT_TYPE);
+	}
 
 	@Bean
 	public SwaggerSerializers swaggerSerializers() {
--- src/main/java/com/github/sbugat/samplerest/config/application/ApplicationConfiguration.java
+++ src/main/java/com/github/sbugat/samplerest/config/application/ApplicationConfiguration.java
@@ -1,28 +1,30 @@
 package com.github.sbugat.samplerest.config.application;
 
-import javax.inject.Inject;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 import com.github.sbugat.samplerest.config.cxf.CXFConfiguration;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 @Configuration
-@PropertySource("classpath:environment/${" + ApplicationConfiguration.ENVIRONMENT_TYPE + ':' + ApplicationConfiguration.DEFAULT_ENVIRONMENT_TYPE + "}/application-configuration.properties")
 @Import(CXFConfiguration.class)
 public class ApplicationConfiguration {
 
-	static final String ENVIRONMENT_TYPE = "environment.type";
-	static final String DEFAULT_ENVIRONMENT_TYPE = "development";
-
-	@Inject
-	private Environment environment;
-
 	@Bean
-	public String environmentType() {
-		return environment.getProperty(ENVIRONMENT_TYPE);
+	public BeanConfig swaggerConfig() {
+		final BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setResourcePackage("com.github.sbugat.samplerest.resource");
+		beanConfig.setVersion("1.0.0");
+		beanConfig.setHost("localhost:8080");
+		beanConfig.setBasePath("/api");
+		beanConfig.setTitle("Swagger CXF, JAX-RS 2.0 sample");
+		beanConfig.setDescription("This is a Swagger CXF, JAX-RS 2.0 sample API.");
+		beanConfig.setContact("Sylvain Bugat");
+		beanConfig.setLicense("Apache 2.0");
+		beanConfig.setLicenseUrl("https://github.com/Sylvain-Bugat/swagger-cxf-rest-skeleton/blob/master/LICENSE");
+		beanConfig.setScan(true);
+		return beanConfig;
 	}
 }
