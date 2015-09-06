08e4d8a57c22e840378a81e8ce00e9612325efec
Sylvain Bugat <cidian@laposte.net>
2015-09-06 17:37:44 +0200

    Add CXF application beans

--- src/main/java/com/github/sbugat/samplerest/config/ApplicationConfig.java
+++ src/main/java/com/github/sbugat/samplerest/config/ApplicationConfig.java
@@ -9,6 +9,12 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.github.sbugat.samplerest.resource.SampleExceptionMapper;
+
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+
 @Configuration
 @ComponentScan("com.github.sbugat.samplerest")
 @PropertySource("classpath:env/${" + ApplicationConfig.ENVIRONMENT_TYPE + ':' + ApplicationConfig.DEFAULT_ENVIRONMENT_TYPE + "}/application-configuration.properties")
@@ -20,9 +26,33 @@ public class ApplicationConfig {
 	@Inject
 	private Environment environment;
 
+	/*
+	 * public ApplicationConfig() { JAXRSServerFactoryBean final Bus cxfBus = BusFactory.getDefaultBus(); cxfBus.getFeatures().add(new LoggingFeature(AbstractLoggingInterceptor.DEFAULT_LIMIT)); }
+	 */
+
 	@Bean
 	public String environmentType() {
 		return environment.getProperty(ENVIRONMENT_TYPE);
+	}
+
+	@Bean
+	public SwaggerSerializers swaggerSerializers() {
+		return new SwaggerSerializers();
+	}
+
+	@Bean
+	public JacksonJaxbJsonProvider jacksonJaxbJsonProvider() {
+		return new JacksonJaxbJsonProvider();
+	}
+
+	@Bean
+	public SampleExceptionMapper sampleExceptionMapper() {
+		return new SampleExceptionMapper();
+	}
+
+	@Bean
+	public ApiListingResource apiListingResource() {
+		return new ApiListingResource();
 	}
 
 	/**
