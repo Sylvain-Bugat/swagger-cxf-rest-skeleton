d98ee35fda3b16930ea7a5e49ac1c1041be6717c
Sylvain Bugat <cidian@laposte.net>
2015-09-06 19:14:43 +0200

    WIP CXF configuration rework

--- src/main/java/com/github/sbugat/samplerest/config/CXFConfiguration.java
+++ src/main/java/com/github/sbugat/samplerest/config/CXFConfiguration.java
@@ -1,58 +1,70 @@
 package com.github.sbugat.samplerest.config;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
+import javax.ws.rs.Path;
 
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import com.github.sbugat.samplerest.resource.BookResource;
 import com.github.sbugat.samplerest.resource.SampleExceptionMapper;
-import com.github.sbugat.samplerest.resource.UserResource;
 import com.google.common.collect.Lists;
 
-import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
 
 @Configuration
 @Import(ApplicationConfig.class)
 public class CXFConfiguration {
 
-	/* Service-resources beans. */
-
+	/** Spring context. */
 	@Inject
-	private UserResource userResource;
+	private ApplicationContext context;
 
-	@Inject
-	private BookResource bookResource;
+	/* Providers. */
 
-	@Inject
-	private ApiListingResource apiListingResource;
-
+	/** Swagger provider. */
 	@Inject
 	private SwaggerSerializers swaggerSerializers;
 
+	/** Jackson JSON provider. */
 	@Inject
 	private JacksonJaxbJsonProvider jacksonJaxbJsonProvider;
 
+	/** Exception handler provider. */
 	@Inject
 	private SampleExceptionMapper sampleExceptionMapper;
 
+	/**
+	 * Contruct the CXF rest-service factory bean.
+	 *
+	 * @return CXF rest-service factory bean
+	 */
 	@Bean(initMethod = "create")
 	public JAXRSServerFactoryBean jAXRSServerFactoryBean() {
 
 		final JAXRSServerFactoryBean jAXRSServerFactoryBean = new JAXRSServerFactoryBean();
 		jAXRSServerFactoryBean.setAddress("/");
+		// Logging feature to display incoming/outgoing messages
 		jAXRSServerFactoryBean.getFeatures().add(new LoggingFeature());
 
-		final List<Object> serviceBeanslist = Lists.newArrayList(userResource, bookResource, apiListingResource);
+		final List<Object> serviceBeanslist = new ArrayList<>();
+		// Get all bean with @Path annotation (resources implementation)
+		for (final Map.Entry<String, Object> entry : context.getBeansWithAnnotation(Path.class).entrySet()) {
+
+			serviceBeanslist.add(entry.getValue());
+		}
+		// Set resource beans
 		jAXRSServerFactoryBean.setServiceBeans(serviceBeanslist);
 
+		// Set providers
 		final List<Object> providerslist = Lists.newArrayList(swaggerSerializers, jacksonJaxbJsonProvider, sampleExceptionMapper);
 		jAXRSServerFactoryBean.setProviders(providerslist);
 
