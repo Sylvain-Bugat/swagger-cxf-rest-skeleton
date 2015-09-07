220c0bcba0b2cb144372ca070cdacbad12d36bfd
Sylvain Bugat <cidian@laposte.net>
2015-09-07 22:15:35 +0200

    WIP secure access configuration

--- src/main/java/com/github/sbugat/samplerest/config/security/SecurityConfiguration.java
+++ src/main/java/com/github/sbugat/samplerest/config/security/SecurityConfiguration.java
@@ -7,16 +7,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.github.sbugat.samplerest.web.security.RestAuthenticationEntryPoint;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Inject
+	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+	@Inject
 	public void configureGlobal(final AuthenticationManagerBuilder authentication) throws Exception {
-		authentication.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		authentication.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin").password("password").roles("ADMIN");
 	}
 
 	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/**").access("hasRole('ROLE_USER')").and().formLogin();
+	protected void configure(final HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.csrf().disable() // Disable CSRF
+				.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and() // Entry point
+				.authorizeRequests().antMatchers("/*").access("hasRole('ROLE_ADMIN')").anyRequest().authenticated() // Admin access
+				.and().authorizeRequests().antMatchers("/api/**").access("hasRole('ROLE_USER')").anyRequest().authenticated() // User access
+				.and().formLogin().loginProcessingUrl("/api/user/login").usernameParameter("username").passwordParameter("password")// login access
+				.and().logout(); // Logout
 	}
 }
--- src/main/java/com/github/sbugat/samplerest/web/security/SpringSecurityWebApplicationInitializer.java
+++ src/main/java/com/github/sbugat/samplerest/web/security/SpringSecurityWebApplicationInitializer.java
@@ -2,6 +2,12 @@ package com.github.sbugat.samplerest.web.security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * Set-up the springSecurityFilterChain bean.
+ *
+ * @author Sylvain Bugat
+ *
+ */
 public class SpringSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 	// do nothing
 }
