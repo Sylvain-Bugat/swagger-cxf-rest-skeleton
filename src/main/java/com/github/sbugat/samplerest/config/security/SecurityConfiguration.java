58a7865f53271c0445efca8609107105e07e7b8c
Sylvain Bugat <cidian@laposte.net>
2015-09-13 18:27:19 +0200

    WIP token authentication

--- src/main/java/com/github/sbugat/samplerest/config/security/SecurityConfiguration.java
+++ src/main/java/com/github/sbugat/samplerest/config/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.github.sbugat.samplerest.web.security.AuthenticationSuccessHandler;
 import com.github.sbugat.samplerest.web.security.RestAuthenticationEntryPoint;
 import com.github.sbugat.samplerest.web.security.TokenAuthenticationFilter;
 
@@ -19,6 +20,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
 	@Inject
+	private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+	@Inject
 	public void configureGlobal(final AuthenticationManagerBuilder authentication) throws Exception {
 		authentication.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin").password("password").roles("ADMIN");
 	}
@@ -26,15 +30,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(final HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.csrf().disable() // Disable CSRF
+		httpSecurity.addFilterAfter(new TokenAuthenticationFilter("/api/**"), UsernamePasswordAuthenticationFilter.class).antMatcher("/api/**") // API token filter
+				.csrf().disable() // Disable CSRF
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Session less
 				.and().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint) // Entry point
-				.and().addFilterBefore(new TokenAuthenticationFilter("/api/**"), UsernamePasswordAuthenticationFilter.class).antMatcher("/api/**").anonymous() // API token filter
-				.and().authorizeRequests().antMatchers("/api/**").access("hasRole('ROLE_USER')").anyRequest().authenticated() // User access
-				.and().authorizeRequests().antMatchers("/api/swagger.json").access("hasRole('ROLE_ADMIN')").anyRequest().authenticated() // Admin access
-				.and().authorizeRequests().antMatchers("/swagger/**").access("hasRole('ROLE_ADMIN')").anyRequest().authenticated() // Admin access
-				.and().formLogin().failureUrl("/login").loginProcessingUrl("/api/user/login").usernameParameter("username").passwordParameter("password") // login access
-				// .and().addFilterBefore(new TokenAuthenticationFilter("/api/**"), UsernamePasswordAuthenticationFilter.class).antMatcher("/api/**").anonymous() // API token filter
+				.and().authorizeRequests().antMatchers("/lib/**").permitAll() //
+				.and().formLogin().successHandler(authenticationSuccessHandler).failureUrl("/login").loginProcessingUrl("/api/user/login").usernameParameter("username").passwordParameter("password") // login access
+				.and().authorizeRequests().antMatchers("/api/**").access("hasRole('ROLE_USER')") // User access
+				.and().authorizeRequests().antMatchers("/api/swagger.json", "/swagger/**").access("hasRole('ROLE_ADMIN')") // Admin access
+				// .and().authorizeRequests().antMatchers("/swagger/**").access("hasRole('ROLE_ADMIN')").anyRequest().authenticated() // Admin access
+
+		// .and().addFilterBefore(new TokenAuthenticationFilter("/api/**"), UsernamePasswordAuthenticationFilter.class).antMatcher("/api/**").anonymous() // API token filter
 				.and().logout(); // Logout
 	}
 }
