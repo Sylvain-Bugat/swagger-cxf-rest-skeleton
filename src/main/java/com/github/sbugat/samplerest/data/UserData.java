9a1f2a4d16d2d4440676e011bc8a31ea1e33de95
Sylvain Bugat <cidian@laposte.net>
2015-09-20 23:59:12 +0200

    WIP add persitence: add admin and roles

--- src/main/java/com/github/sbugat/samplerest/data/UserData.java
+++ src/main/java/com/github/sbugat/samplerest/data/UserData.java
@@ -16,14 +16,17 @@ public class UserData {
 
 	@PostConstruct
 	public void initializeData() {
-		userDao.save(createUser("user1", "password1", "user1@domain.com", "lastname1", "firstname1", 1));
-		userDao.save(createUser("user2", "password2", "user2@domain.com", "lastname2", "firstname2", 5));
-		userDao.save(createUser("user3", "password3", "user3@domain.com", "lastname3", "firstname3", 3));
-		userDao.save(createUser("user4", "password4", "user4@domain.com", "lastname4", "firstname4", 4));
-		userDao.save(createUser("user5", "password5", "user5@domain.com", "lastname5", "firstname5", 1));
+		userDao.save(createUser("admin1", "password1", "user1@domain.com", "lastname1", "firstname1", "ROLE_USER, ROLE_ADMIN", 1));
+		userDao.save(createUser("admin2", "password2", "user2@domain.com", "lastname2", "firstname2", "ROLE_USER, ROLE_ADMIN", 2));
+
+		userDao.save(createUser("user1", "password1", "user1@domain.com", "lastname1", "firstname1", "ROLE_USER", 1));
+		userDao.save(createUser("user2", "password2", "user2@domain.com", "lastname2", "firstname2", "ROLE_USER", 2));
+		userDao.save(createUser("user3", "password3", "user3@domain.com", "lastname3", "firstname3", "ROLE_USER", 3));
+		userDao.save(createUser("user4", "password4", "user4@domain.com", "lastname4", "firstname4", "ROLE_USER", 4));
+		userDao.save(createUser("user5", "password5", "user5@domain.com", "lastname5", "firstname5", "ROLE_USER", 1));
 	}
 
-	private static User createUser(final String username, final String password, final String email, final String lastName, final String firstName, final int userStatus) {
+	private static User createUser(final String username, final String password, final String email, final String lastName, final String firstName, final String roles, final int userStatus) {
 
 		final User user = new User();
 		user.setUsername(username);
@@ -31,6 +34,7 @@ public class UserData {
 		user.setEmail(email);
 		user.setFirstName(firstName);
 		user.setLastName(lastName);
+		user.setRoles(roles);
 		user.setUserStatus(userStatus);
 
 		return user;
