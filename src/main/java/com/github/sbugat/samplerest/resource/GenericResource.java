2504dee40bf50731f1ba8e161a6ab7f1ec6ace3a
Sylvain Bugat <cidian@laposte.net>
2015-09-23 21:10:36 +0200

    Add REST GenericResource and versionning

---
+++ src/main/java/com/github/sbugat/samplerest/resource/GenericResource.java
@@ -0,0 +1,20 @@
+package com.github.sbugat.samplerest.resource;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.github.sbugat.samplerest.service.OrikaBeanMapper;
+
+@Consumes({ GenericResource.API_VERSION1_MEDIA_TYPE, GenericResource.API_VERSION2_MEDIA_TYPE })
+@Produces(MediaType.APPLICATION_JSON)
+public abstract class GenericResource {
+
+	protected final static String API_VERSION1_MEDIA_TYPE = "application/apiv1+json";
+
+	protected final static String API_VERSION2_MEDIA_TYPE = "application/apiv2+json";
+
+	@Inject
+	protected OrikaBeanMapper orikaBeanMapper;
+}
--- src/main/java/com/github/sbugat/samplerest/resource/UserResource.java
+++ src/main/java/com/github/sbugat/samplerest/resource/UserResource.java
@@ -2,55 +2,53 @@ package com.github.sbugat.samplerest.resource;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import com.github.sbugat.samplerest.dao.UserDao;
 import com.github.sbugat.samplerest.dto.UserDto;
 import com.github.sbugat.samplerest.exception.ApiException;
+import com.github.sbugat.samplerest.exception.BadRequestException;
 import com.github.sbugat.samplerest.exception.NotFoundException;
 import com.github.sbugat.samplerest.model.User;
-import com.github.sbugat.samplerest.service.OrikaBeanMapper;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
 
 @Path("/user")
-@Api(
-		value = "/user",
-		authorizations = { @Authorization("api_token") })
-@Consumes({ "application/json" })
-@Produces({ "application/json" })
+@Api("/user")
 @Named
-public class UserResource {
+public class UserResource extends GenericResource {
 
 	@Inject
 	private UserDao userDao;
-
-	@Inject
-	OrikaBeanMapper orikaBeanMapper;
 
 	@POST
 	@ApiOperation(
 			value = "Create user",
 			notes = "This can only be done by the logged in user.")
+	@ApiResponses(
+			value = @ApiResponse(
+					code = 400,
+					message = "Invalid user supplied") )
 	public Response createUser(@ApiParam(
 			value = "Created user object",
-			required = true) final UserDto userDto) {
+			required = true) final UserDto userDto) throws BadRequestException {
 		final User user = orikaBeanMapper.map(userDto, User.class);
-		userDao.save(user);
-		return Response.ok().entity("").build();
+		try {
+			userDao.save(user);
+		} catch (final Exception e) {
+			throw new BadRequestException(400, "Error inserting user");
+		}
+		return Response.ok().build();
 	}
 
 	@PUT
