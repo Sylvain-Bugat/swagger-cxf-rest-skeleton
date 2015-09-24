859e87f88603daf35e91b47ee746f26c773a26ce
Sylvain Bugat <cidian@laposte.net>
2015-09-24 23:12:49 +0200

    Add @Produces on GenericResource

--- src/main/java/com/github/sbugat/samplerest/resource/GenericResource.java
+++ src/main/java/com/github/sbugat/samplerest/resource/GenericResource.java
@@ -3,12 +3,11 @@ package com.github.sbugat.samplerest.resource;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import com.github.sbugat.samplerest.service.OrikaBeanMapper;
 
 @Consumes({ GenericResource.API_VERSION1_MEDIA_TYPE, GenericResource.API_VERSION2_MEDIA_TYPE })
-@Produces(MediaType.APPLICATION_JSON)
+@Produces({ GenericResource.API_VERSION1_MEDIA_TYPE, GenericResource.API_VERSION2_MEDIA_TYPE })
 public abstract class GenericResource {
 
 	protected final static String API_VERSION1_MEDIA_TYPE = "application/apiv1+json";
